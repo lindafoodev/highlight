4209d899d56d2ee2e26e47bdad94baf30d81fb16
Linda Foo <linda.foo.dev@gmail.com>
2018-08-15 16:23:08 -0700

    correct file name typo, bg on body; add image boxes with style for d+m

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,33 @@ export default class Home extends React.Component {
   render(){
       return (
           <div className='main'>
-             <a href='' className='box m1n1'/>
+            <div className='box-container theme-color-1 col-4'>
+                <div className='inner-box-container m1n1'></div>
+            </div>
+            <div className='box-container theme-color-2 col-4'>
+                <div className='inner-box-container m1n2'></div>
+            </div>
+            <div className='box-container theme-color-3 col-4'>
+                <div className='inner-box-container m1n3'></div>
+            </div>
+            <div className='box-container theme-color-3 col-4'>
+                <div className='inner-box-container m2n1'></div>
+            </div>
+            <div className='box-container theme-color-1 col-4'>
+                <div className='inner-box-container m2n2'></div>
+            </div>
+            <div className='box-container theme-color-2 col-4'>
+                <div className='inner-box-container m2n3'></div>
+            </div>
+            <div className='box-container theme-color-2 col-4'>
+                <div className='inner-box-container m3n1'></div>
+            </div>
+            <div className='box-container theme-color-3 col-4'>
+                <div className='inner-box-container m3n2'></div>
+            </div>
+            <div className='box-container theme-color-1 col-4'>
+                <div className='inner-box-container m3n3'></div>
+            </div>
           </div>  
       );
   }
