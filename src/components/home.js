832130077e88d40302238915eecc2f449094aca7
Linda Foo <linda.foo.dev@gmail.com>
2018-08-16 18:29:03 -0700

    add main + box overlay, set-up modal, hover effect in progress

--- src/components/home.js
+++ src/components/home.js
@@ -5,33 +5,49 @@ export default class Home extends React.Component {
   render(){
       return (
           <div className='main'>
-            <div className='box-container theme-color-1 col-4'>
-                <div className='inner-box-container m1n1'></div>
-            </div>
-            <div className='box-container theme-color-2 col-4'>
-                <div className='inner-box-container m1n2'></div>
-            </div>
-            <div className='box-container theme-color-3 col-4'>
-                <div className='inner-box-container m1n3'></div>
-            </div>
-            <div className='box-container theme-color-3 col-4'>
-                <div className='inner-box-container m2n1'></div>
-            </div>
-            <div className='box-container theme-color-1 col-4'>
-                <div className='inner-box-container m2n2'></div>
-            </div>
-            <div className='box-container theme-color-2 col-4'>
-                <div className='inner-box-container m2n3'></div>
-            </div>
-            <div className='box-container theme-color-2 col-4'>
-                <div className='inner-box-container m3n1'></div>
-            </div>
-            <div className='box-container theme-color-3 col-4'>
-                <div className='inner-box-container m3n2'></div>
-            </div>
-            <div className='box-container theme-color-1 col-4'>
-                <div className='inner-box-container m3n3'></div>
-            </div>
+                <div className='hidden-on-mobile box-container theme-color-1 col-4'>
+                    <div className='inner-box-bg m1n1'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div className='hidden-on-mobile box-container theme-color-2 col-4'>
+                    <div className='inner-box-bg m1n2'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div className='hidden-on-mobile box-container theme-color-3 col-4'>
+                    <div className='inner-box-bg m1n3'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div className='box-container theme-color-3 col-4'>
+                    <div className='inner-box-bg m2n1'></div>
+                    <div className='overlay'><a href='#openModal-registry' className='hvr-grow style-text style-text-short'>Registry</a></div>
+                </div>
+                <div className='box-container theme-color-1 col-4'>
+                    <div className='inner-box-bg m2n2'></div>
+                    <div className='overlay'><a href='#openModal-eventDetails' className='hvr-grow style-text style-text-long'>Event Details</a></div>
+                </div>
+                <div className='box-container theme-color-2 col-4'>
+                    <div className='inner-box-bg m2n3'></div>
+                    <div className='overlay'><a href='#openModal-countdown' className='hvr-grow style-text'>Countdown</a></div>
+                </div>
+                <div className='hidden-on-mobile box-container theme-color-2 col-4'>
+                    <div className='inner-box-bg m3n1'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div className='hidden-on-mobile box-container theme-color-3 col-4'>
+                    <div className='inner-box-bg m3n2'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div className='hidden-on-mobile box-container theme-color-1 col-4'>
+                    <div className='inner-box-bg m3n3'></div>
+                    <div className='overlay-hide'></div>
+                </div>
+                <div id='openModal-registry' className='modalDialog'>
+                    <div>
+                        <a href='#close' title='Close' className='close'>X</a>
+                        <h2 className='text'>Registry</h2>
+                        <a href="www.amazon.com">Amazon</a>
+                    </div>
+                </div>
           </div>  
       );
   }
