ac4360300484705580daeed52fde2b04e9952bb1
Linda Foo <linda.foo.dev@gmail.com>
2018-08-20 10:38:07 -0700

    set-up countdown modal

--- src/components/home.js
+++ src/components/home.js
@@ -64,7 +64,7 @@ export default class Home extends React.Component {
                             <img className='logo' src={bbb} alt='BuyBuy Baby Registry logo'/>
                         </a>
                     </div>
-                    <p>Shiwani and Animesh are registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> & <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
+                    <p>Shiwani & Animesh are registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> and <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
                 </div>
             </div>
             <div id='openModal-eventDetails' className='modalDialog'>
@@ -80,9 +80,20 @@ export default class Home extends React.Component {
                     <p className='section-text higher-emphasis'>550 Moreland Way</p>
                     <p className='section-text higher-emphasis'>Santa Clara, Ca 95054</p>
                     <p className='section-text break-text'>Hosted by Olivia & Linda</p>
-                    <p className='section-text'>Regrets only to Linda</p>
+                    <p className='break-text'>Regrets only to Linda</p>
                     <p className='section-text'>713.822.4934</p>
-                    <p className='break-text'>Registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> & <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
+                    <p className='break-text'>Registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> and <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
+                </div>
+            </div>
+            <div id='openModal-countdown' className='modalDialog'>
+                <div className='theme-color-1'>
+                    <a href='#close' title='Close' className='close'>X</a>
+                    <h2 className='style-text'>Countdown</h2>
+                    <div className='countdown'>
+                       
+                    </div>
+                    <p className='section-text'>Arrival Date</p>
+                    <p className='section-text'>October 18, 2018</p>
                 </div>
             </div>
           </div>  
