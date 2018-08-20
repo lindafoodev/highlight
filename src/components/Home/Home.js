9514a89a5e53397949bbb99f1e5ef9f8671ac2d4
Linda Foo <linda.foo.dev@gmail.com>
2018-08-20 12:54:54 -0700

    up - simplified href section ref

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ export default class Home extends React.Component {
                     </div>
                     <div className='box-container theme-color-2 col-4'>
                         <div className='inner-box-bg m2n1'></div>
-                        <div className='overlay'><a href='#openModal-registry' className='hvr-grow style-text'>Registry</a></div>
+                        <div className='overlay'><a href='#registry' className='hvr-grow style-text'>Registry</a></div>
                     </div>
                     <div className='hidden-on-mobile box-container theme-color-3 col-4'>
                         <div className='inner-box-bg m3n1'></div>
@@ -36,7 +36,7 @@ export default class Home extends React.Component {
                     </div>
                     <div className='box-container theme-color-1 col-4'>
                         <div className='inner-box-bg m2n2'></div>
-                        <div className='overlay'><a href='#openModal-eventDetails' className='hvr-grow style-text'>Event Details</a></div>
+                        <div className='overlay'><a href='#event' className='hvr-grow style-text'>Event Details</a></div>
                     </div>
                     <div className='hidden-on-mobile box-container theme-color-2 col-4'>
                         <div className='inner-box-bg m3n2'></div>
@@ -50,7 +50,7 @@ export default class Home extends React.Component {
                     </div>
                     <div className='box-container theme-color-3 col-4'>
                         <div className='inner-box-bg m2n3'></div>
-                        <div className='overlay'><a href='#openModal-countdown' className='hvr-grow style-text'>Countdown</a></div>
+                        <div className='overlay'><a href='#countdown' className='hvr-grow style-text'>Countdown</a></div>
                     </div>
                     <div className='hidden-on-mobile box-container theme-color-1 col-4'>
                         <div className='inner-box-bg m3n3'></div>
@@ -58,7 +58,7 @@ export default class Home extends React.Component {
                     </div>
                 </div>
             </div>
-            <div id='openModal-registry' className='modalDialog'>
+            <div id='registry' className='modalDialog'>
                 <div className='theme-color-1'>
                     <a href='#close' title='Close' className='close'>X</a>
                     <h2 className='style-text'>Registry</h2>
@@ -73,7 +73,7 @@ export default class Home extends React.Component {
                     <p>Shiwani & Animesh are registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> and <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
                 </div>
             </div>
-            <div id='openModal-eventDetails' className='modalDialog'>
+            <div id='event' className='modalDialog'>
                 <div className='theme-color-3'>
                     <a href='#close' title='Close' className='close'>X</a>
                     <p className='section-text'>Please join us for a</p>
@@ -91,7 +91,7 @@ export default class Home extends React.Component {
                     <p className='break-text'>Registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> and <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
                 </div>
             </div>
-            <div id='openModal-countdown' className='modalDialog'>
+            <div id='countdown' className='modalDialog'>
                 <div className='theme-color-1'>
                     <a href='#close' title='Close' className='close'>X</a>
                     <h2 className='style-text'>Countdown</h2>
