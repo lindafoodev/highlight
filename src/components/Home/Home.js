629ab97de898cbddd980ef32654ab680e186bd7c
Linda Foo <linda.foo.dev@gmail.com>
2018-08-21 11:31:14 -0700

    up modal padding, event modal style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -88,7 +88,7 @@ export default class Home extends React.Component {
                         <p className='break-text emphasis-text'>Clubhouse</p>
                         <p className='section-text emphasis-text'>550 Moreland Way</p>
                         <p className='section-text emphasis-text'>Santa Clara, Ca 95054</p>
-                        <p className='section-text break-text'>Hosted by Olivia & Linda</p>
+                        <p className='attention-text section-text break-text'>Hosted by Olivia & Linda</p>
                         <a href='#rsvp' className='btn-submit hvr-grow'>RSVP</a>
                         <p className='break-text'>Registered at <a href='https://www.amazon.com/baby-reg/animesh-agarwal-shiwani-gupta-october-2018-sanbruno/38M35EETLQPXV' rel='noopener noreferrer' target='_blank' title='Amazon Registry'>Amazon</a> and <a href='https://www.buybuybaby.com:443/store/giftregistry/view_registry_guest.jsp?registryId=546350683&eventType=Baby&pwsurl=' rel='noopener noreferrer' target='_blank' title='buybuy Baby Registry'>buybuy Baby</a></p>
                     </div>
