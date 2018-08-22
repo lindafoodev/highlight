b0892ad91824dacb775b9dc9b4913000ae08f4ce
Linda Foo <linda.foo.dev@gmail.com>
2018-08-21 19:28:03 -0700

    reorg rsvp form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -26,22 +26,6 @@ class Form extends Component {
                 <input required id="last" type="text" name="last" placeholder="Doe" />
               </fieldset>
 
-              <fieldset className="block">
-              <legend>Can you attend?<span className={"direction-text"}>Required</span></legend>
-              <input id="rsvp" className='radio' type="radio" name="rsvp" value="Yes" required/> <label className="rsvp" htmlFor="rsvp">Yes, woohoo!</label>
-              <input id="rsvp" className='radio' type="radio" name="rsvp" value="No" /> <label className="rsvp" htmlFor="rsvp">No, there in spirit</label>
-              </fieldset>
-
-              <fieldset>
-                <label htmlFor="guestCount">No. of Guests Attending</label><span className={"direction-text"}>Required</span>
-                <input required id="guestCount" type="number" title="Input a valid number" name="guestCount" defaultValue="1" />
-              </fieldset>              
-              
-              <fieldset>
-                <label htmlFor="otherGuests">Other Guest Names</label><span className={"direction-text"}>Optional</span>
-                <input id="otherGuests" type="text" name="otherGuests" placeholder="Shiwani, Animesh" />
-              </fieldset>  
-
               <fieldset>
                 <legend>Contact Info</legend>
                 <label htmlFor="email">Email Address</label><span className={"direction-text"}>Required</span>
@@ -49,24 +33,40 @@ class Form extends Component {
                 placeholder="your.name@email.com"/>
                 <span className="email-invalid" style={{display: 'none'}}>
                   Must be a valid email address</span>
+              </fieldset>              
+
+              <fieldset className="block">
+              <legend>Can you attend?<span className={"direction-text"}>Required</span></legend>
+              <input id="rsvp" className='radio' type="radio" name="rsvp" value="Yes" required/> <label className="rsvp" htmlFor="rsvp">Yes, woohoo!</label>
+              <input id="rsvp" className='radio' type="radio" name="rsvp" value="No" /> <label className="rsvp" htmlFor="rsvp">No, there in spirit.</label>
               </fieldset>
 
               <fieldset>
+                <label htmlFor="guestCount">No. of Guests With You</label>
+                <input id="guestCount" type="number" title="Input a valid number" name="guestCount" defaultValue="0" />
+              </fieldset>              
+              
+              <fieldset>
+                <label htmlFor="otherGuests">Other Guest Names</label>
+                <input id="otherGuests" type="text" name="otherGuests" placeholder="Shiwani, Animesh" />
+              </fieldset>  
+
+              <fieldset>
                 <legend>Mailing Address</legend>
-                <label htmlFor="address1">Address 1</label><span className={"direction-text"}>Required</span>
-                <input required type="text" id="address1" name="address1" placeholder="123 Safari Blvd" />
-                <label htmlFor="address2">Address 2</label><span className={"direction-text"}>Optional</span>
+                <label htmlFor="address1">Address 1</label>
+                <input type="text" id="address1" name="address1" placeholder="123 Safari Blvd" />
+                <label htmlFor="address2">Address 2</label>
                 <input id="address2" type="text" name="address2" placeholder="Suite 1A" />
-                <label htmlFor="city">City</label><span className={"direction-text"}>Required</span>
-                <input required id="city" type="text" name="city" placeholder="San Francisco" />
-                <label htmlFor="state">State</label><span className={"direction-text"}>Required</span>
-                <input required type="text" id="state" name="state" placeholder="CA" />
-                <label htmlFor="zip">Zip</label><span className={"direction-text"}>Required</span>
-                <input required type="text" pattern="[0-9]*" id="zip" name="zip" placeholder="12345" />
+                <label htmlFor="city">City</label>
+                <input id="city" type="text" name="city" placeholder="San Francisco" />
+                <label htmlFor="state">State</label>
+                <input type="text" id="state" name="state" placeholder="CA" />
+                <label htmlFor="zip">Zip</label>
+                <input type="text" pattern="[0-9]*" id="zip" name="zip" placeholder="12345" />
               </fieldset>          
 
               <fieldset>
-              <legend>Message<span className={"direction-text"}>Optional</span></legend>
+              <legend>Message</legend>
                 <textarea id="comments" name="comments" rows="3"
                 placeholder="Have any questions or comments..."></textarea>
               </fieldset>              
