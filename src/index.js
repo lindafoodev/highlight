618eff07f4204bb4a3b44c169e266a05fe0edde8
Linda Foo <linda.foo.dev@gmail.com>
2018-08-27 20:14:05 -0700

    disable registerServiceWorker; add unregister so users get latest update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/Home/Home';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+unregister();
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
